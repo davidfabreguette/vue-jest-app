b5a08235de96da50807408d1f2c63d7fc0eccfc1
David FABREGUETTE <david@macbook-pro-de-david.home>
2019-12-29 07:32:19 +0100

    first commit

---
+++ app/javascript/tests/myspecs.spec.js
@@ -0,0 +1,12 @@
+import hello_vue from "../app.vue"
+
+describe("hello-vue", () => {
+
+  describe("chocolate", () => {
+    it("has a chocolate method", () => {
+      expect("this").toBe("this")
+      // const vm = new Vue(about).$mount()
+      // expect(vm.chocolate()).toBe("good");
+    })
+  })
+})
